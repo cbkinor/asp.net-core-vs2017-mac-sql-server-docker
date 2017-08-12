4df5dbb73bc3bf368f0dc0f42e29c4b18e6fea61
Chris <chris.kuiper@gmail.com>
2017-08-12 09:03:50 -0700

    use scoffolding and set up connections for sql server in docker

---
+++ MvcMovieSql/Models/MvcMovieSqlContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcMovieSql.Models
+{
+	public class MvcMovieSqlContext : DbContext
+	{
+		public MvcMovieSqlContext(DbContextOptions<MvcMovieSqlContext> options)
+			: base(options)
+		{
+		}
+
+		public DbSet<MvcMovieSql.Models.Movie> Movie { get; set; }
+	}
+}
--- MvcMovieSql/Startup.cs
+++ MvcMovieSql/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using MvcMovieSql.Models;
 
 namespace MvcMovieSql
 {
@@ -29,7 +31,11 @@ namespace MvcMovieSql
         {
             // Add framework services.
             services.AddMvc();
-        }
+
+			services.AddEntityFrameworkSqlServer()
+					.AddDbContext<MvcMovieSqlContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:MvcMovieSqlDB"]));
+
+		}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
