75308866bd1c6ad2b6e25da6ecf0fe7668132ccc
Chris <chris.kuiper@gmail.com>
2017-08-13 09:33:42 -0700

    add search functionality

--- MvcMovieSql/Models/Movie.cs
+++ MvcMovieSql/Models/Movie.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace MvcMovieSql.Models
@@ -7,7 +7,8 @@ namespace MvcMovieSql.Models
 	{
 		public int ID { get; set; }
 		public string Title { get; set; }
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Release Date")]
+		[DataType(DataType.Date)]
 		public DateTime ReleaseDate { get; set; }
 		public string Genre { get; set; }
 		public decimal Price { get; set; }
---
+++ MvcMovieSql/Models/MovieGenreViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MvcMovieSql.Models
+{
+    public class MovieGenreViewModel
+    {
+		public List<Movie> movies;
+	    public SelectList genres;
+	    public string movieGenre { get; set; }
+    }
+}
